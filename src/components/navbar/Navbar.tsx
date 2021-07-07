668c9e9c7f822012db101609c1932c3eba5755aa
Graner007 <graner.bence007@gmail.com>
2021-07-07 14:30:02 +0200

    load data from json file in dropdown menu list

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,5 @@
-import { Input, Menu, Space } from 'antd';
+import { Input, Menu } from 'antd';
 import { Component } from 'react';
-import Logo from "./Logo";
 import Dropdown from "./Dropdown";
 import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 
@@ -10,8 +9,8 @@ export default class Navbar extends Component {
         return (
             <Menu theme="light" mode="horizontal">
                 <Menu.Item key="1">Home</Menu.Item>
-                <Menu.Item key="2"><Dropdown title="Shop By Artist" items={['Elvis Presley', 'Michael Jackson', 'Frank Sinatra']} /></Menu.Item>
-                <Menu.Item key="3"><Dropdown title="Shop By Category" items={['Pop', 'Jazz', 'Metal']} /></Menu.Item>
+                <Menu.Item key="2"><Dropdown title="Shop By Artist" itemName="artist" /></Menu.Item>
+                <Menu.Item key="3"><Dropdown title="Shop By Category" itemName="genre" /></Menu.Item>
                 <Menu.Item key="4"><Search placeholder="input search text" allowClear style={{ width: 200, paddingTop: 17 }} /></Menu.Item>
                 <Menu.Item key="5" style={{marginLeft: "auto"}}><ShoppingCartOutlined /></Menu.Item>
                 <Menu.Item key="6"><UserOutlined /></Menu.Item>
