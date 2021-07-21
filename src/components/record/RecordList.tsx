91e098e6d448ff23eeeb323c98e43be25ef49ae1
Graner007 <graner.bence007@gmail.com>
2021-07-21 11:12:16 +0200

    add shadow to cart items and style to icons

--- src/components/record/RecordList.tsx
+++ src/components/record/RecordList.tsx
@@ -29,7 +29,7 @@ const RecordList = ({records, maxWidth}: Props) => {
                         className="shadow"
                         maxWidth={ maxWidth }
                         cover={ <img src={item.albumCover} alt="cover" /> } 
-                        actions={[<ShoppingCartOutlined />, <HeartOutlined />]}>
+                        actions={[ <ShoppingCartOutlined style={{color: "green", fontSize: 20}} />, <HeartOutlined style={{color: "red", fontSize: 20}} /> ]}>
                             <Meta title={<Link to={"/products/" + item.name.split(" ").join("-").toLowerCase()}>{item.name}</Link>} description={"by " + item.artist + " for " + item.price + "$"} />
                     </Card>
                 </List.Item>
