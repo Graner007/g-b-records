7921c6d2874d1b1ce4e71b69c490b0da8f2587a9
Graner007 <graner.bence007@gmail.com>
2021-09-03 10:55:48 +0200

    fix update quantity price

--- src/components/user/cart/ListCart.tsx
+++ src/components/user/cart/ListCart.tsx
@@ -1,20 +1,22 @@
 import { Component } from 'react';
-import { List, InputNumber, Avatar } from "antd";
+import { List, InputNumber, Avatar, message } from "antd";
 import { DeleteFilled } from '@ant-design/icons';
 import { Link } from "react-router-dom";
 import { ChildProps, graphql } from "@apollo/react-hoc";
-import { gql } from '@apollo/client';
+import { ApolloCache, ApolloError, gql } from '@apollo/client';
 
 import { CartModel } from "../../../components/interface/CartModel";
 import EmptyDescription from "../../warning/EmptyDescription";
 import { P } from "../../Styles";
 import { CartItemModel } from '../../interface/CartItemModel';
+import { CART_QUERY, CartType } from './Cart';
 
 const UPDATE_CARTITEM_QUANTITY_MUTATION = gql`
     mutation UpdateCartItemQuantity($cartItemId: Int!, $cartItemQuantity: Int!) {
         updateCartItemQuantity(cartItemId: $cartItemId, cartItemQuantity: $cartItemQuantity) {
             name   
             quantity
+            price
         }
     }
 `;
@@ -25,7 +27,7 @@ type UpdateCartItemQuantityVars = {
 }
 
 type CartItemType = {
-    cartItem: CartItemModel;
+    updateCartItemQuantity: CartItemModel;
 }
 
 type Props = {
@@ -52,7 +54,38 @@ class ListCart extends Component<ChildProps<Props, CartItemType, UpdateCartItemQ
                         avatar={<Link to={"/products/" + item.name.toLowerCase().replaceAll(" ", "-")}><Avatar src={item.albumCover} shape="square" size="large" /></Link>}
                     />
                     { this.props.editable ? 
-                        <InputNumber onChange={e => this.props.mutate && this.props.mutate({ variables: { cartItemId: item.id, cartItemQuantity: e.valueOf() } }).then((res) => console.log(res.data)).catch((err) => console.error(err))} min={0} defaultValue={item.quantity} size="large" /> : 
+                        <InputNumber 
+                        onChange={e => {
+                            this.props.mutate && 
+                            this.props.mutate({ 
+                                variables: { 
+                                    cartItemId: parseInt(String(item.id)), 
+                                    cartItemQuantity: e.valueOf() 
+                                },
+                                update: (proxy, { data }) => {
+                                    const cart = proxy.readQuery<CartType>({ query: CART_QUERY });
+
+                                    if (data && cart) {
+                                        if (data.updateCartItemQuantity.quantity === 0) {
+                                            cart.cart.cart.products.filter(product => product.id === data.updateCartItemQuantity.id);
+                                        }
+                                        else {
+                                            cart.cart.cart.products.forEach(product => {
+                                                if (product.id === data.updateCartItemQuantity.id) {
+                                                    product.price = data.updateCartItemQuantity.price * data.updateCartItemQuantity.quantity;
+                                                }
+                                            });
+                                        }
+                                        proxy.writeQuery({ query: CART_QUERY, data: cart });
+                                    }
+                                }
+                            })
+                                .then(() => message.success("Record quantity updated"))
+                                .catch((err: ApolloError) => message.error(err.message));
+                        }} 
+                        min={0} 
+                        defaultValue={item.quantity} 
+                        size="large" /> : 
                         <P fontsize={20}>{item.quantity}X</P> 
                     }
                 </List.Item>
